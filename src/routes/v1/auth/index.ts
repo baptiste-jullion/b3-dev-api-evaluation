a1f3e72f20e042a67b7f03074b65e6312db463ed
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-02-02 20:07:18 +0100

    refactor: handleBody -> validateBody

--- src/routes/v1/auth/index.ts
+++ src/routes/v1/auth/index.ts
@@ -2,15 +2,11 @@ import { Router } from "express";
 import { login } from "#c/v1/auth/login";
 import { register } from "#c/v1/auth/register";
 import { ZUserWriteLogin } from "#db/models/User";
-import { handleBody } from "#m/validation";
+import { validateBody } from "#m/validation";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
-authRouter.post(
-	"/login",
-	(req, res, next) => handleBody(ZUserWriteLogin, req, res, next),
-	login,
-);
+authRouter.post("/login", validateBody(ZUserWriteLogin), login);
 
 export { authRouter };
--- src/routes/v1/products/index.ts
+++ src/routes/v1/products/index.ts
@@ -5,21 +5,17 @@ import { getProduct } from "#c/v1/products/get";
 import { listProducts } from "#c/v1/products/list";
 import { updateProduct } from "#c/v1/products/update";
 import { ZProductWrite } from "#db/models/Product";
-import { handleBody } from "#m/validation";
+import { validateBody } from "#m/validation";
 
 const productsRouter = Router();
 
 productsRouter.get("/", listProducts);
-productsRouter.post(
-	"/",
-	(req, res, next) => handleBody(ZProductWrite, req, res, next),
-	createProduct,
-);
+productsRouter.post("/", validateBody(ZProductWrite), createProduct);
 productsRouter.get("/:id", getProduct);
 productsRouter.delete("/:id", deleteProduct);
 productsRouter.patch(
 	"/:id",
-	(req, res, next) => handleBody(ZProductWrite.partial(), req, res, next),
+	validateBody(ZProductWrite.partial()),
 	updateProduct,
 );
 
